ea4e7e83063e9bb5015f0ef40c04de163f45e710
DangerStep <103132813+bguehbguegureguer@users.noreply.github.com>
2023-01-22 22:08:52 -0800

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -7,6 +7,6 @@ public static class ModHelperData
 
     public const string Description = "Adds a game mode where it is almost impossible to lose. Good luck...";
 
-    public const string RepoOwner = "Kai"; // TODO add your github username hero, also in the download url in README.md
-    public const string RepoName = "NoRepo"; // TODO add your repo name here, also in the download url in README.md
+    public const string RepoOwner = "bguehbguegureguer"; // TODO add your github username hero, also in the download url in README.md
+    public const string RepoName = "too-easy-too-lost-btd6-mod"; // TODO add your repo name here, also in the download url in README.md
 }
